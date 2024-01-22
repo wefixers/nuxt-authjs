b43e48f65f4726e6aec1ca3e330b87582452e0fa
lucacicada <100947658+lucacicada@users.noreply.github.com>
2024-01-22 15:05:41 +0100

    Fix useAuth typings

--- src/runtime/plugin.server.ts
+++ src/runtime/plugin.server.ts
@@ -1,9 +1,10 @@
+import type { NuxtApp } from '#app'
 import { defineNuxtPlugin, useAuth } from '#imports'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   // Skip auth if we're prerendering
   if (!process.prerender) {
-    const { data, getSession } = useAuth(nuxtApp)
+    const { data, getSession } = useAuth(nuxtApp as NuxtApp)
 
     if (typeof data.value === 'undefined') {
       await getSession()
