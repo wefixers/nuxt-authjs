e34870fb0b27a7ef5245fd21e11996466804638d
lucacicada <100947658+lucacicada@users.noreply.github.com>
2024-01-22 15:04:10 +0100

    Refactor auth middleware

--- src/runtime/middleware/auth.ts
+++ src/runtime/middleware/auth.ts
@@ -1,45 +1,55 @@
 import type { PublicModuleOptions } from '../../module'
 import { defineNuxtRouteMiddleware, navigateTo, useAuth, useRuntimeConfig } from '#imports'
 
-type MiddlewareMeta = boolean | {
+interface MiddlewareMeta {
   /**
    * Whether to only allow unauthenticated users to access this page.
    *
    * Authenticated users will be redirected to `/` or the route defined in `navigateAuthenticatedTo`
    *
-   * @default undefined
+   * @default true
    */
   unauthenticatedOnly?: boolean
+
   /**
    * Where to redirect authenticated users if `unauthenticatedOnly` is set to true
    *
    * @default undefined
    */
   navigateAuthenticatedTo?: string
-  /**
-   * Where to redirect unauthenticated users if this page is protected
-   *
-   * @default undefined
-   */
-  navigateUnauthenticatedTo?: string
 }
 
 declare module '#app' {
   interface PageMeta {
     /**
      * Configure authentication for this page.
+     *
+     * - `false` will disable the auth middleware for this page
+     * - `true` will use the default configuration
      */
-    auth?: MiddlewareMeta
+    auth?: boolean | MiddlewareMeta
+  }
+}
+
+function getAuthMiddlewareMeta(auth: unknown): MiddlewareMeta | false {
+  if (auth === false) {
+    return false
+  }
+
+  if (typeof auth === 'object') {
+    return {
+      unauthenticatedOnly: true,
+      ...auth,
+    }
+  }
+
+  return {
+    unauthenticatedOnly: true,
   }
 }
 
 export default defineNuxtRouteMiddleware((to) => {
-  const metaAuth = typeof to.meta.auth === 'object'
-    ? {
-        unauthenticatedOnly: true,
-        ...to.meta.auth,
-      }
-    : to.meta.auth
+  const metaAuth = getAuthMiddlewareMeta(to.meta.auth)
 
   // shortcut, user have explicitly disabled the auth middleware
   if (metaAuth === false) {
@@ -48,7 +58,7 @@ export default defineNuxtRouteMiddleware((to) => {
 
   const { status } = useAuth()
 
-  const isGuestMode = typeof metaAuth === 'object' && metaAuth.unauthenticatedOnly
+  const isGuestMode = metaAuth.unauthenticatedOnly
 
   // Guest mode happy path 1: Unauthenticated user is allowed to view page
   if (isGuestMode && status.value === 'unauthenticated') {
@@ -56,7 +66,7 @@ export default defineNuxtRouteMiddleware((to) => {
   }
 
   // Guest mode edge-case: Developer used guest-mode config style but set `unauthenticatedOnly` to `false`
-  if (typeof metaAuth === 'object' && !metaAuth.unauthenticatedOnly) {
+  if (!metaAuth.unauthenticatedOnly) {
     return
   }
 
@@ -93,7 +103,7 @@ export default defineNuxtRouteMiddleware((to) => {
       query: {
         ...to.query,
         error: 'SessionRequired',
-        callbackUrl: to.path,
+        callbackUrl: to.fullPath,
       },
     })
   }
@@ -103,7 +113,7 @@ export default defineNuxtRouteMiddleware((to) => {
     query: {
       ...to.query,
       error: 'SessionRequired',
-      callbackUrl: to.path,
+      callbackUrl: to.fullPath,
     },
   }, {
     external: true,
